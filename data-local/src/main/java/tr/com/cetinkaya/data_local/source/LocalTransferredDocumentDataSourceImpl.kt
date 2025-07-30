592c3d6255938f694608940338a7f3a5c0a1f31a
Enver ASLAN <easlan@cetinkaya.com.tr>
2025-07-30 14:23:01 +0300

    refactor: LocalStockTransactionDataSource.kt
    overload updateStockTransactionSyncStatus: It has 7 parameters
    (
    transactionType: Byte,
    transactionKind: Byte,
    isNormalOrReturn: Byte,
    documentType: Byte,
    documentSeries: String,
    documentNumber: Int,
    syncStatus: String
    )

--- data-local/src/main/java/tr/com/cetinkaya/data_local/source/LocalTransferredDocumentDataSourceImpl.kt
+++ data-local/src/main/java/tr/com/cetinkaya/data_local/source/LocalTransferredDocumentDataSourceImpl.kt
@@ -1,7 +1,10 @@
 package tr.com.cetinkaya.data_local.source
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import tr.com.cetinkaya.common.enums.TransferredDocumentTypes
 import tr.com.cetinkaya.data_local.db.dao.TransferredDocumentDao
+import tr.com.cetinkaya.data_local.db.entities.toDataModel
 import tr.com.cetinkaya.data_local.db.entities.toEntity
 import tr.com.cetinkaya.data_repository.datasource.local.LocalTransferredDocumentDataSource
 import tr.com.cetinkaya.data_repository.models.transferred_document.TransferredDocumentDataModel
@@ -9,9 +12,19 @@ import javax.inject.Inject
 
 class LocalTransferredDocumentDataSourceImpl @Inject constructor(
     private val transferredDocumentDao: TransferredDocumentDao,
-) : LocalTransferredDocumentDataSource{
+) : LocalTransferredDocumentDataSource {
 
     override suspend fun add(transferredDocument: TransferredDocumentDataModel): Long {
         return transferredDocumentDao.add(transferredDocument.toEntity())
     }
+
+    override suspend fun delete(
+        transferredDocumentTypes: TransferredDocumentTypes, documentSeries: String, documentNumber: Int
+    ): Int {
+        return transferredDocumentDao.delete(documentSeries, documentNumber, transferredDocumentTypes)
+
+    }
+
+    override fun getUntransferredDocuments(): Flow<List<TransferredDocumentDataModel>> =
+        transferredDocumentDao.getUntransferredDocuments().map { transferredDocuments -> transferredDocuments.map { it.toDataModel() } }
 }
