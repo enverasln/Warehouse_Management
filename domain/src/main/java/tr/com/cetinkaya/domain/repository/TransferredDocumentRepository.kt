09976aaf40efcaabec3ed1f269fea2621eb3b8ab
Enver ASLAN <easlan@cetinkaya.com.tr>
2025-07-30 14:16:21 +0300

    refactor: TransferredDocumentRepository.kt
    Add new function to delete transferred document
    Change add function as suspend

--- domain/src/main/java/tr/com/cetinkaya/domain/repository/TransferredDocumentRepository.kt
+++ domain/src/main/java/tr/com/cetinkaya/domain/repository/TransferredDocumentRepository.kt
@@ -2,15 +2,23 @@ package tr.com.cetinkaya.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 import tr.com.cetinkaya.common.enums.TransferredDocumentTypes
+import tr.com.cetinkaya.domain.model.transferred_document.TransferredDocumentDomainModel
 
 interface TransferredDocumentRepository {
 
-
-    fun add(
+    suspend fun add(
         transferredDocumentType: TransferredDocumentTypes,
         documentSeries: String,
         documentNumber: Int,
         synchronizationStatus: Boolean,
         description: String
-    ): Flow<Long>
+    ): Long
+
+    suspend fun delete(
+        transferredDocumentTypes: TransferredDocumentTypes,
+        documentSeries: String,
+        documentNumber: Int
+    ) : Int
+
+    fun getUntransferredDocuments(): Flow<List<TransferredDocumentDomainModel>>
 }
