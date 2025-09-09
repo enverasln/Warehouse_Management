8d9d5603bae7b56ceb522e74489cf9e96254a6ec
Enver ASLAN <easlan@cetinkaya.com.tr>
2025-09-09 15:45:41 +0300

    feat(WarehouseUiModel): add new function
    toDomainModel function converts warehouse ui model to domain model

--- feature-goods-transfer/src/main/java/tr/com/cetinkaya/feature_goods_transfer/warehouse_transfer/models/WarehouseUiModel.kt
+++ feature-goods-transfer/src/main/java/tr/com/cetinkaya/feature_goods_transfer/warehouse_transfer/models/WarehouseUiModel.kt
@@ -3,20 +3,14 @@ package tr.com.cetinkaya.feature_goods_transfer.warehouse_transfer.models
 import tr.com.cetinkaya.domain.model.warehouse.WarehouseDomainModel
 
 data class WarehouseUiModel(
-    val id: String,
-    val number: Int,
-    val name: String,
-    val isActive: Boolean
+    val id: String, val warehouseNumber: Int, val name: String, val isActive: Boolean
 ) {
 
     override fun toString(): String {
         return name
     }
 }
+fun WarehouseDomainModel.toUiModel() = WarehouseUiModel(id = this.id, warehouseNumber = this.warehouseNumber, name = this.name, isActive = this.isActive)
 
-fun WarehouseDomainModel.toUiModel() = WarehouseUiModel(
-    id = this.id,
-    number = this.warehouseNumber,
-    name = this.name,
-    isActive = this.isActive
-)
+fun WarehouseUiModel.toDomainModel() = WarehouseDomainModel(id = this.id, warehouseNumber = this.warehouseNumber, name = this.name, isActive = this.isActive)
+
